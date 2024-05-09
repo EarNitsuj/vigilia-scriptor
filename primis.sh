368a0c163acdc800bc0bf3d20379e3a5c2418075
EarNitsuj <157909601+EarNitsuj@users.noreply.github.com>
2024-05-09 20:54:08 +0200

    Update primis.sh

--- primis.sh
+++ primis.sh
@@ -1,1 +1,6 @@
-
+sudo apt-get install --yes curl git openssl && \
+curl -fsSL get.docker.com -o ${HOME}/get-docker.sh
+sudo sh ${HOME}/get-docker.sh && \
+sudo systemctl enable --now docker
+sudo usermod -aG docker $(whoami)
+sudo -i -u $(whoami) -- /path/to/post_login_commands.sh
