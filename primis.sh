28e46923c9118da7bbc20b17c393945c2882ec66
EarNitsuj <157909601+EarNitsuj@users.noreply.github.com>
2024-05-19 21:00:36 +0200

    Update primis.sh

--- primis.sh
+++ primis.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+# Install curl, git, and openssl
 sudo apt-get install --yes curl git openssl && \
-curl -fsSL get.docker.com -o ${HOME}/get-docker.sh
+# Download Docker installation script
+curl -fsSL get.docker.com -o ${HOME}/get-docker.sh && \
+# Execute Docker installation script
 sudo sh ${HOME}/get-docker.sh && \
-sudo systemctl enable --now docker
-sudo usermod -aG docker $(whoami)
+# Enable and start Docker service
+sudo systemctl enable --now docker && \
+# Add current user to the Docker group
+sudo usermod -aG docker $(whoami) && \
+# Switch to current user and run secundo.sh script
 sudo -i -u $(whoami) -- ./secundo.sh
