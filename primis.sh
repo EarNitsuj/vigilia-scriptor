75c9c8092b547951e4545c0d9e4649b915deb7a9
EarNitsuj <157909601+EarNitsuj@users.noreply.github.com>
2024-05-29 12:52:27 +0200

    Update primis.sh

--- primis.sh
+++ primis.sh
@@ -2,25 +2,35 @@
 
 # Install curl, git, and openssl
 sudo apt-get install --yes curl git openssl && \
+echo -e "Install curl, git, and openss \n\n"  && \
 
 # Download Docker installation script
 curl -fsSL get.docker.com -o ${HOME}/get-docker.sh && \
+echo -e "Download Docker installation script \n\n"  && \
 
 # Execute Docker installation script
 sudo sh ${HOME}/get-docker.sh && \
+echo -e "Execute Docker installation script \n\n"  && \
 
 # Set 2 minute sleep
 sleep 100 && \
+echo -e "Set 2 minute sleep \n\n"  && \
 
 # Enable and start Docker service
 sudo systemctl enable --now docker && \
+echo -e "Enable and start Docker service \n\n"  && \
 
 # Set 5 seconds sleep
 sleep 5 && \
+echo -e "Set 5 seconds sleep \n\n"  && \
 
 # Add current user to the Docker group
 sudo usermod -aG docker $(whoami) && \
+echo -e "Add current user to the Docker group \n\n"  && \
 
 # Switch to current user
-sudo -i -u $(whoami)
+sudo -i -u $(whoami) && \
+echo -e "Switch to current user \n\n"  && \
+
+./secundo.sh
 
